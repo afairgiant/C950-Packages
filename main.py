5c6208eb4ab53168cc90903eab5097c4ef10b869
Alex <alex.j.fair@gmail.com>
2024-02-28 11:20:25 -0500

    Load Package Data Fixed

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 
 import csv
 
+from trio import open_file
+
 from hashtable import ChainingHashTable
 from package import Package
 from truck import Truck
@@ -29,43 +31,45 @@ CSV_Address = read_csv(ADDRESS_FILE)
 # print(f" Address \n {CSV_Address} \n")
 
 # Read the package information file
-Package_Data = read_csv(PACKAGE_FILE)
+CSV_Package = read_csv(PACKAGE_FILE)
 # print(f"Packages \n {Package_Data} \n")
 
 # Initiate hash table
 PackageHashTable = ChainingHashTable()
 
 
-# Read csv files, and put into hash table
-def loadPackageData(Package_Data, PackageHashTable):
-    print("loadPackageData called with {} packages".format(len(Package_Data)))
-    # CSV_Package
-    for package in Package_Data[1:]:
-        package_id = int(package[0])
-        address = package[1]
-        city = package[2]
-        state = package[3]
-        zipcode = package[4]
-        deadline_time = package[5]
-        weight = package[6]  # in KG
-        status = "At Hub"
-        note = package[7]
+# Read package csv file, and put into hash table
+def loadPackageData(packageFile):
+    with open(packageFile) as packageList:
+        packageData = csv.reader(packageList, delimiter=",")
+        next(packageData)  # skip header
+        for package in packageData:
+            package_id = int(package[0])
+            address = package[1]
+            city = package[2]
+            state = package[3]
+            zipcode = package[4]
+            deadline_time = package[5]
+            weight = package[6]  # in KG
+            status = "At Hub"
+            note = package[7]
 
-        # Package Object
-        package_object = Package(
-            package_id,
-            address,
-            city,
-            state,
-            zipcode,
-            deadline_time,
-            weight,
-            status,
-            note,
-        )
-        print(f"package {package_object}")
-        # Add each package to hash table
-        PackageHashTable.insert(package_id, package_object)
+            # Package Object
+            package_object = Package(
+                package_id,
+                address,
+                city,
+                state,
+                zipcode,
+                deadline_time,
+                weight,
+                status,
+                note,
+            )
+            print(f"package {package_object}")
+            # Add each package to hash table
+            PackageHashTable.insert(package_id, package_object)
 
 
+loadPackageData(PACKAGE_FILE)
 print(PackageHashTable.print_table())
